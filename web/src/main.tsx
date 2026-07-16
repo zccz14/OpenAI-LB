89d8aa33f0887a84a03ab87dea57e1d7daf53b85
CZ <cz@ntnl.io>
2026-07-16 16:08:25 +0800

    🐛 Fix Auth Mini hosted login

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,11 +4,14 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { consumeAuthMiniCallback } from "@/lib/auth-redirect.ts"
+
+const authRedirectError = consumeAuthMiniCallback()
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <App startupError={authRedirectError} />
     </ThemeProvider>
   </StrictMode>
 )
